25c96bffbc716177461c690ca73c05a0a31eacbf
BOUZDUG Zakaria <zakariabouzdig@gmail.com>
2025-03-14 03:41:19 +0100

    GestionCandidatures.java

--- src/beans/GestionCandidatures.java
+++ src/beans/GestionCandidatures.java
@@ -1,5 +1,36 @@
 package beans;
-
+import java.util.ArrayList;
+import java.util.List;
 public class GestionCandidatures {
+    private List<Master> masters = new ArrayList<>();
+    private List<Candidat> candidats = new ArrayList<>();
+    private List<Candidature> candidatures = new ArrayList<>();
 
+    public void ajouterMaster(Master master) {
+        masters.add(master);
+    }
+    public void enregistrerCandidature(Candidature candidature) {
+        candidatures.add(candidature);
+    }
+    public List<Candidature> filtrerParFiliere(String filiere) {
+        List<Candidature> result = new ArrayList<>();
+        for (Candidature c : candidatures) {
+            for (Master m : masters) {
+                if (m.getId() == c.getMasterId() && m.getFiliere().equalsIgnoreCase(filiere)) {
+                    result.add(c);
+                }
+            }
+        }
+        return result;
+    }
+
+    public Candidat rechercherCandidat(String nom) {
+        for (Candidat c : candidats) {
+            if (c.getNom().equalsIgnoreCase(nom)) {
+                return c;
+            }
+        }
+        return null;
+    }
 }
+
