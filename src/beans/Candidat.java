15033435aeb84cbc0b4bda0f3e1ed48e064baf10
BOUZDUG Zakaria <zakariabouzdig@gmail.com>
2025-03-14 03:38:43 +0100

    Candidat.java

--- src/beans/Candidat.java
+++ src/beans/Candidat.java
@@ -1,5 +1,20 @@
 package beans;
-
 public class Candidat {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String diplome;
+    private String mention;
 
+    public Candidat(int id, String nom, String prenom, String diplome, String mention) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.diplome = diplome;
+        this.mention = mention;
+    }
+
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+    public String getPrenom() { return prenom; }
 }
