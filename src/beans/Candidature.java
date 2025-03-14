b8b8a94b7868c2e121c0f59ac554448e6f9cb000
BOUZDUG Zakaria <zakariabouzdig@gmail.com>
2025-03-14 03:40:47 +0100

    Candidature.java

--- src/beans/Candidature.java
+++ src/beans/Candidature.java
@@ -1,5 +1,17 @@
 package beans;
-
 public class Candidature {
+private int id;
+private int masterId;
+private int candidatId;
+private String statut;
+    public Candidature(int id, int masterId, int candidatId, String statut) {
+        this.id = id;
+        this.masterId = masterId;
+        this.candidatId = candidatId;
+        this.statut = statut;
+    }
 
+    public int getId() { return id; }
+    public int getMasterId() { return masterId; }
+    public int getCandidatId() { return candidatId; }
 }
