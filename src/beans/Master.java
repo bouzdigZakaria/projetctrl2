5d194bdfc3bd84dd350a9c97c17ea7be1f7cb404
BOUZDUG Zakaria <zakariabouzdig@gmail.com>
2025-03-14 03:42:01 +0100

    Master.java

--- src/beans/Master.java
+++ src/beans/Master.java
@@ -1,5 +1,19 @@
 package beans;
-
 public class Master {
-
-}
+private int id;
+private String intitule;
+private String filiere;
+private int placesDisponibles;
+    public Master(int id, String intitule, String filiere, int placesDisponibles) {
+        this.id = id;
+        this.intitule = intitule;
+        this.filiere = filiere;
+        this.placesDisponibles = placesDisponibles;
+    }
+    public int getId() { return id; }
+    public String getIntitule() { return intitule; }
+    public String getFiliere() { return filiere; }
+    public int getPlacesDisponibles() { return placesDisponibles; }
+    public void setPlacesDisponibles(int placesDisponibles) { 
+    	this.placesDisponibles = placesDisponibles; }
+    }
